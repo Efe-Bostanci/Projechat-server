9bc3c63ca734d65770e707ddb4680f3d88f9d3b3
Efe Bostancı <efebostanci.eb@gmail.com>
2023-11-18 23:55:05 +0300

    Update chat.js

--- chat.js
+++ chat.js
@@ -6,21 +6,36 @@ server.listen(3000, () => {
 });
 
 const io = require('socket.io')(server);
-const connections = [];
+
+// Odaları saklamak için bir nesne oluştur
+const rooms = {};
 
 io.on('connection', (socket) => {
     console.log('Yeni bağlantı');
-    connections.push(socket);
 
-    socket.on('message', (message) => {
-        connections.forEach(element => {
-            if (element !== socket)
-                element.emit('message', message);
-        });
+    socket.on('joinRoom', (room) => {
+        // Kullanıcıyı belirtilen odaya katılmasını sağla
+        socket.join(room);
+
+        // Odaya katılan kullanıcılara bildirim gönder
+        io.to(room).emit('message', 'Kullanıcı katıldı: ' + socket.id);
+    });
+
+    socket.on('message', (data) => {
+        // İlgili odaya mesajı gönder
+        io.to(data.room).emit('message', data.message);
     });
 
     socket.on('disconnect', () => {
         console.log('Bağlantı kapatıldı');
-        connections.splice(connections.indexOf(socket), 1);
+
+        // Kullanıcının ayrıldığı odaları bul
+        const roomsLeft = Object.keys(socket.rooms);
+
+        // Her odadan kullanıcıyı çıkar
+        roomsLeft.forEach(room => {
+            socket.leave(room);
+            io.to(room).emit('message', 'Kullanıcı ayrıldı: ' + socket.id);
+        });
     });
 });
